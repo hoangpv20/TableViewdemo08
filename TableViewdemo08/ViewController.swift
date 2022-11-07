d4ae3d468919d7409effbc676b6adda24f12e721
hoangpv20 <viethoang211200@gmail.com>
2022-11-07 13:13:14 +0700

    fix title

--- TableViewdemo08/ViewController.swift
+++ TableViewdemo08/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let data: [Animal] = [
+    Animal(imageName: "bird", title: "Bird"),
+    Animal(imageName: "bird", title: "Bird"),
+    Animal(imageName: "bird", title: "Bird"),
+    Animal(imageName: "bird", title: "Bird"),
 
+    ]
+
+    @IBOutlet weak var table: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let animal = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.loadData(item: animal)
+//        if (indexPath.row%2 == 0){
+//            cell.label.textColor = .red
+//        }
+        cell.redTitle(number: indexPath)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+struct Animal{
+    let imageName: String
+    let title: String
 }
 
